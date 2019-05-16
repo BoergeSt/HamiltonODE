cfc50c2c1b1be5c68a6d7f581ec1067646cc441f
BoergeSt <boerge.struempfel@tum.de>
2019-05-16 19:08:13 +0200

    Fixed Julia file-ending

---
+++ HamiltonODE.jl
@@ -0,0 +1,8 @@
+
+include("HamiltonODE/HamiltonSystem.jl")
+include("HamiltonODE/HamiltonSteps.jl")
+include("HamiltonODE/HamiltonIntegrator.jl")
+include("HamiltonODE/HamiltonPlot.jl")
+
+println("============================")
+println("Finished loading HamiltonODE")
