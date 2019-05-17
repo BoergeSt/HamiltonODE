c527d55a3d41a2034b90065ef6d3bb2d9016e38b
BoergeSt <boerge.struempfel@tum.de>
2019-05-17 20:48:05 +0200

    Added Error Estimation for the Lennard-Jones-Trimer

--- HamiltonODE.jl
+++ HamiltonODE.jl
@@ -3,6 +3,7 @@ include("HamiltonODE/HamiltonSystem.jl")
 include("HamiltonODE/HamiltonSteps.jl")
 include("HamiltonODE/HamiltonIntegrator.jl")
 include("HamiltonODE/HamiltonPlot.jl")
+include("HamiltonODE/HamiltonInterpolate.jl")
 
 println("============================")
 println("Finished loading HamiltonODE")
--- HamiltonODE/HamiltonInterpolate.jl
+++ HamiltonODE/HamiltonInterpolate.jl
@@ -3,12 +3,16 @@ function linearSplineInterpolation(fineT,fineX,T)
     if length(T)>1
         return map(T -> linearSplineInterpolation(fineT,fineX,T),T)
     end
-    i = findfirst(fineT.>T)
+    i = findfirst(fineT.>=T)
     if isnothing(i)
-        return fineX[end]
+        return fineX[:,end]
     end
     if i<=1
-        return fineX[1]
+        return fineX[:,1]
     end
-    return fineX[i-1]+(T-fineT[i-1])/(fineT[i]-fineT[i-1])*(fineX[i]-fineX[i-1])
+    return fineX[:,i-1]+(T-fineT[i-1])/(fineT[i]-fineT[i-1])*(fineX[:,i]-fineX[:,i-1])
 end
+
+
+
+println("Finished loading HamiltonInterpolate")
