d00fdde67f019688553e253c0fb79a2b0407cfc5
BoergeSt <boerge.struempfel@tum.de>
2019-05-17 14:48:45 +0200

    Added kwargs to plotT

--- HamiltonODE/HamiltonPlot.jl
+++ HamiltonODE/HamiltonPlot.jl
@@ -4,18 +4,18 @@ function plotOrbits(Qs,names="";kwargs...)
     return plot(X,Y,label=hcat(names...);kwargs...)
 end
 
-function plotT(ts,Qs,titles)
+function plotT(ts,Qs,titles;kwargs...)
     p=[]
     for (t,Q,title) in zip(ts,Qs,titles)
-        push!(p,plot(t,Q',title=title))
+        push!(p,plot(t,Q',title=title);kwargs...)
     end
     return plot(p...)
 end
 
-plotT(ts,Qs,title::String)=plotT(ts,Qs,title,1:length(ts))
-plotT(ts,Qs,title::String,extension)=plotT(ts,Qs,["$title $(extension[i])" for i in 1:length(ts)])
+plotT(ts,Qs,title::String;kwargs...)=plotT(ts,Qs,title,1:length(ts);kwargs...)
+plotT(ts,Qs,title::String,extension;kwargs...)=plotT(ts,Qs,["$title $(extension[i])" for i in 1:length(ts)];kwargs...)
 
-plotT(ts,Qs)=plotT(ts,Qs,"plot ")
+plotT(ts,Qs;kwargs...)=plotT(ts,Qs,"plot ";kwargs...)
 
 
 
